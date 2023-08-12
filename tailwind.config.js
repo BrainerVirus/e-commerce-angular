203064e0a90862fb63524565f1e96c73555fcc20
Cristhofer Pincetti <cristhoferpincetti@hotmail.cl>
2023-08-12 16:49:42 -0400

    feat(home-page): dark/light themes had been updated
    the color palettes for both themes has been updated and implemented

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,23 @@ module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {
+      boxShadow: {
+        inset: "inset 0 0px 5px rgba(0, 0, 0, 0.2)",
+      },
       colors: {
         bubble_bg: "#007cff2e",
         "ocean-blue-dark-theme": {
           dark: "#041725",
           medium: "#0C2340",
           light: "#02101E",
-          accent: "#00cde9",
+          accent: "#0076DD",
         },
         "ocean-blue-light-theme": {
-          light: "#2683c8",
-          medium: "#0861b9",
-          accent: "#00cde9",
+          dark: "#004fca",
+          medium: "#003a93",
+          light: "#0064ff",
+          accent: "#41d2fa",
+          accent_nav_hover: "#24A3DD",
         },
       },
     },
